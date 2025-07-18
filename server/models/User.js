4f437bda5596e138126472728047875cce37c584
Garv Tiwari <garvtwr37@gmail.com>
2025-07-18 02:52:18 -0700

    backend/user/model/imageSchema/updated

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,7 @@ const userSchema = mongoose.Schema({
   _id: { type: String, required: true },
    username: { type: String, required: true },
   email:{type:String,required : true},
-  image:{type:String,required : true},
+  image:{type:String},
   role:{type:String,enum:["user","hotelOwenr"],default:"user"},
   recentSearchedCities:[{type:String,required:true}],
 },{timestamps:true});
